ab746ec4a86d081cef4b11253726c223e12f455c
Kirill <k.matviychuck@gmail.com>
2023-01-10 12:40:21 +0200

    change message

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-    let message = 'Test server is running';
+    let message = 'Test server running';
     res.send(message)
 })
 
